eb16b06d44263138d2f56a41e2f0b991200d4745
Bryson Shea <bryson.w.shea@gmail.com>
2022-03-05 10:40:19 -1000

    Remove existing 10 questions if they click. Fix the look a lil

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const url = 'https://opentdb.com/api.php?amount=10' 
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 async function getTriviaFunction(num){
     const response = await fetch(url)
     const jsonData = await response.json()
 
     const divvy = document.getElementById('bulletlist')
+    removeAllChildNodes(divvy)
     const jokeList = document.createElement('ol')
 
     for(let i = 0; i<num;i++){
@@ -12,14 +19,16 @@ async function getTriviaFunction(num){
         joke.setAttribute('class','number')
         const question = document.createElement('div')
         const answer = document.createElement('div')
+        const lineBreak = document.createElement('br')
 
-        question.innerText = `Question: ${jsonData.results[i].question}\n`
+        question.innerText = `Question: ${jsonData.results[i].question}`
         question.setAttribute('class','question')
 
         answer.setAttribute('class','answer')
         answer.innerText = `Answer: ${jsonData.results[i].correct_answer}`
 
         joke.appendChild(question)
+        answer.appendChild(lineBreak)
         joke.appendChild(answer)
         jokeList.appendChild(joke)
     }
